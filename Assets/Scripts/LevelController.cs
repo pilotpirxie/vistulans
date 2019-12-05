a00e81e35fa79b4a2261cc3b295eb877ebecd63f
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2019-12-06 00:50:21 +0100

    Instantiating at position from level config

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -3,11 +3,18 @@
 public class LevelController : MonoBehaviour
 {
 
+    public GameObject VertexObject;
+
     void Start()
     {
         TextAsset levelConfigContent = Resources.Load<TextAsset>("Config/levels");
         Debug.Log($"Loaded level configuration: {levelConfigContent}");
         LevelConfig levelConfig = JsonUtility.FromJson<LevelConfig>(levelConfigContent.text);
+
+        foreach(VertexProxy vertexProxy in levelConfig.levels[0].vertexProxies)
+        {
+            Object.Instantiate(VertexObject, new Vector3(vertexProxy.x * 1f, 0.5f, vertexProxy.y * 1f), Quaternion.identity);
+        }
     }
 
 }
