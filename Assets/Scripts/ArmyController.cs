f34ded3e6a28fda8e2149f88ccdea409c90ac726
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2019-12-06 22:04:30 +0100

    Move. increment, fight and destroy armies

--- Assets/Scripts/ArmyController.cs
+++ Assets/Scripts/ArmyController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class ArmyController : MonoBehaviour
 {
     [SerializeField]
+    public int Origin = -1;
+
+    [SerializeField]
     public int Target = -1;
 
     [SerializeField]
@@ -15,6 +18,9 @@ public class ArmyController : MonoBehaviour
 
     [SerializeField]
     public OwnerType Owner = OwnerType.Wild;
+
+    [SerializeField]
+    private bool AlreadyTriggering = false;
 
     void UpdateTarget(int newTarget)
     {
@@ -34,6 +40,66 @@ public class ArmyController : MonoBehaviour
             targetDirection.y = 0;
             transform.rotation = Quaternion.LookRotation(targetDirection);
             gameObject.transform.position += gameObject.transform.forward * 0.1f;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.gameObject.GetComponent<ArmyController>().AlreadyTriggering = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+
+        if (AlreadyTriggering == false)
+        {
+            if (other.gameObject.tag == "Vertex")
+            {
+                if (other.gameObject.GetComponent<VertexController>().Owner == Owner && other.gameObject.GetComponent<VertexController>().Id != Origin)
+                {
+                    other.gameObject.GetComponent<VertexController>().ArmyPower += ArmyPower;
+                }
+                else
+                {
+                    other.gameObject.GetComponent<VertexController>().ArmyPower -= ArmyPower;
+
+                    if (other.gameObject.GetComponent<VertexController>().ArmyPower <= 0)
+                    {
+                        other.gameObject.GetComponent<VertexController>().Owner = Owner;
+                        other.gameObject.GetComponent<VertexController>().ArmyPower = Mathf.Abs(other.gameObject.GetComponent<VertexController>().ArmyPower);
+                    }
+                }
+
+                GameObject.Destroy(gameObject);
+
+            }
+            else if (other.gameObject.tag == "Army")
+            {
+                if (other.gameObject.GetComponent<ArmyController>().AlreadyTriggering == false && other.gameObject.GetComponent<ArmyController>().Owner != Owner)
+                {
+                    other.gameObject.GetComponent<ArmyController>().AlreadyTriggering = true;
+
+                    if (other.gameObject.GetComponent<ArmyController>().ArmyPower < ArmyPower)
+                    {
+                        ArmyPower -= other.gameObject.GetComponent<ArmyController>().ArmyPower;
+
+                        GameObject.Destroy(other.gameObject);
+
+                    }
+                    else if (other.gameObject.GetComponent<ArmyController>().ArmyPower > ArmyPower)
+                    {
+                        other.gameObject.GetComponent<ArmyController>().ArmyPower -= ArmyPower;
+
+                        GameObject.Destroy(gameObject);
+                    }
+                    else
+                    {
+                        GameObject.Destroy(gameObject);
+                        GameObject.Destroy(other.gameObject);
+                    }
+                }
+            }
         }
     }
 }
