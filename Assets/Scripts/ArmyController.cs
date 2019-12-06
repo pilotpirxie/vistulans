cdda74141178f230eda6751ff6279f8a40e6b302
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2019-12-06 21:05:56 +0100

    Rotate army toward target node

--- Assets/Scripts/ArmyController.cs
+++ Assets/Scripts/ArmyController.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class ArmyController : MonoBehaviour
 {
     [SerializeField]
-    public int From = -1;
+    public int Target = -1;
 
     [SerializeField]
-    public int To = -1;
+    private GameObject TargetObject;
 
     [SerializeField]
     public int ArmyPower = 0;
@@ -16,13 +16,24 @@ public class ArmyController : MonoBehaviour
     [SerializeField]
     public OwnerType Owner = OwnerType.Wild;
 
-    void Start()
+    void UpdateTarget(int newTarget)
     {
-        
+        Target = newTarget;
+        TargetObject = GameObject.Find($"vertex{Target}");
     }
 
     void FixedUpdate()
     {
-        
+        if (TargetObject == null)
+        {
+            UpdateTarget(Target);
+        }
+        else
+        {
+            Vector3 targetDirection = TargetObject.gameObject.transform.position - transform.position;
+            targetDirection.y = 0;
+            transform.rotation = Quaternion.LookRotation(targetDirection);
+            gameObject.transform.position += gameObject.transform.forward * 0.1f;
+        }
     }
 }
