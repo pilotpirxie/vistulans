6ffff5d50ffc9f2ea39d8cbf9a3f7e2a45a76e33
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2020-02-01 23:38:08 +0100

    Refactor and write docs for Badge Controller

--- Assets/Scripts/BadgeController.cs
+++ Assets/Scripts/BadgeController.cs
@@ -3,58 +3,119 @@ using UnityEngine;
 
 public class BadgeController : MonoBehaviour
 {
+    /// <summary>
+    /// Level to display
+    /// </summary>
     public int Level;
+
+    /// <summary>
+    /// Army power to display
+    /// </summary>
     public int ArmyPower;
+
+    /// <summary>
+    /// Type of vertex to display
+    /// </summary>
     public VertexType Type;
+
+    /// <summary>
+    /// Owner to display
+    /// </summary>
     public OwnerType Owner;
 
-    public GameObject LevelText;
-    public GameObject PowerText;
-    public GameObject TypeText;
-    public GameObject Background;
+    /// <summary>
+    /// Label with level
+    /// </summary>
+    public TextMeshProUGUI LevelText;
 
+    /// <summary>
+    /// Label with army power 
+    /// </summary>
+    public TextMeshProUGUI ArmyPowerText;
+
+    /// <summary>
+    /// Label with vertex type
+    /// </summary>
+    public TextMeshProUGUI TypeText;
+
+    /// <summary>
+    /// Sprite with background image
+    /// </summary>
+    public SpriteRenderer Background;
+
+    /// <summary>
+    /// Prefab of image used for wild owner type
+    /// </summary>
     public Sprite WildBackground;
+
+    /// <summary>
+    /// Prefab of image used for player owner
+    /// </summary>
     public Sprite PlayerBackground;
+
+    /// <summary>
+    /// Prefab of image used for first enemy owner
+    /// </summary>
     public Sprite EnemyOneBackground;
+
+    /// <summary>
+    /// Prefab of image used for second enemy owner
+    /// </summary>
     public Sprite EnemyTwoBackground;
+
+    /// <summary>
+    /// Prefab of image used for third enemy owner
+    /// </summary>
     public Sprite EnemyThreeBackground;
 
     private void FixedUpdate()
     {
-        if (LevelText.GetComponent<TextMeshProUGUI>().text != $"Lv. {Level}")
+        SetLabels();
+        SetBackground();
+    }
+
+    /// <summary>
+    /// Set badge UI labels
+    /// </summary>
+    void SetLabels()
+    {
+        if (TypeText.text != $"Lv. {Level}")
         {
-            LevelText.GetComponent<TextMeshProUGUI>().text = $"Lv. {Level}";
+            TypeText.text = $"Lv. {Level}";
         }
 
-        if (LevelText.GetComponent<TextMeshProUGUI>().text != ArmyPower.ToString())
+        if (ArmyPowerText.text != ArmyPower.ToString())
         {
-            PowerText.GetComponent<TextMeshProUGUI>().text = ArmyPower.ToString();
+            ArmyPowerText.text = ArmyPower.ToString();
         }
 
-        if (LevelText.GetComponent<TextMeshProUGUI>().text != Type.ToString()[0].ToString())
+        if (TypeText.text != Type.ToString()[0].ToString())
         {
-            TypeText.GetComponent<TextMeshProUGUI>().text = Type.ToString()[0].ToString();
+            TypeText.text = Type.ToString()[0].ToString();
         }
+    }
 
+    /// <summary>
+    /// Change badge background sprite
+    /// </summary>
+    void SetBackground()
+    {
         switch (Owner)
         {
             case OwnerType.Wild:
-                Background.GetComponent<SpriteRenderer>().sprite = WildBackground;
+                Background.sprite = WildBackground;
                 break;
             case OwnerType.Player:
-                Background.GetComponent<SpriteRenderer>().sprite = PlayerBackground;
+                Background.sprite = PlayerBackground;
                 break;
-
             case OwnerType.EnemyOne:
-                Background.GetComponent<SpriteRenderer>().sprite = EnemyOneBackground;
+                Background.sprite = EnemyOneBackground;
                 break;
-
             case OwnerType.EnemyTwo:
-                Background.GetComponent<SpriteRenderer>().sprite = EnemyTwoBackground;
+                Background.sprite = EnemyTwoBackground;
                 break;
-
             case OwnerType.EnemyThree:
-                Background.GetComponent<SpriteRenderer>().sprite = EnemyThreeBackground;
+                Background.sprite = EnemyThreeBackground;
                 break;
         }
     }
