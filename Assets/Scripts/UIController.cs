f988cb8e88940061b821c44fd68480c8a210df3f
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2020-02-01 23:01:56 +0100

    Refactor UI Controller

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,24 +1,66 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
 public class UIController : MonoBehaviour
 {
+    /// <summary>
+    /// Upgrade button with information about 
+    /// amount of honey to upgrade
+    /// </summary>
     public Button Upgrade;
+
+    /// <summary>
+    /// Button for casting offensive spell
+    /// </summary>
     public Button SpellOffensive;
+
+    /// <summary>
+    /// Button for casting earthquake
+    /// </summary>
     public Button SpellEarthquake;
+
+    /// <summary>
+    /// Button for casting takeover spell
+    /// </summary>
     public Button SpellTakeover;
 
+    /// <summary>
+    /// Label with information about current time scale
+    /// </summary>
     public TextMeshProUGUI SpeedText;
+
+    /// <summary>
+    /// Label with information about current part
+    /// of army that will be send between vertices
+    /// </summary>
     public TextMeshProUGUI TransportPartText;
+
+    /// <summary>
+    /// Label with amount of honey
+    /// required for upgrade
+    /// </summary>
     public TextMeshProUGUI UpgradeCostText;
+
+    /// <summary>
+    /// Label with info about amount of player honey
+    /// </summary>
     public TextMeshProUGUI HoneyText;
+
+    /// <summary>
+    /// Label with info about player mana
+    /// </summary>
     public TextMeshProUGUI ManaText;
 
+    /// <summary>
+    /// Image object used as overlay on the screen
+    /// when player spell is choosen
+    /// </summary>
     public Image CastingSpellVignette;
 
+    /// <summary>
+    /// Reference to gameplay controller in Mechanism object
+    /// </summary>
     private GameplayController _gameplayController;
 
     private void Start()
@@ -26,12 +68,23 @@ public class UIController : MonoBehaviour
         _gameplayController = GameObject.FindWithTag("Mechanism").GetComponent<GameplayController>();
     }
 
-    void Update()
+    /// <summary>
+    /// Set text value of labels on the screen
+    /// </summary>
+    void SetPlayerUI()
     {
-        HoneyText.text = _gameplayController.Honey[0].ToString();
-        ManaText.text = _gameplayController.Mana[0].ToString();
+        HoneyText.text = _gameplayController.Honey[1].ToString();
+        ManaText.text = _gameplayController.Mana[1].ToString();
+        TransportPartText.text = $"{_gameplayController.TransportPart * 100}%";
+        SpeedText.text = $"x{_gameplayController.GameplaySpeedMultiplier}";
+    }
 
-
+    /// <summary>
+    /// Check if only one vertex is selected and if level is below 5
+    /// then activate or not upgrade button
+    /// </summary>
+    void SetUpgradeButton()
+    {
         if (_gameplayController.SelectedVertexA != null && _gameplayController.SelectedVertexA.Level < 5)
         {
             Upgrade.gameObject.SetActive(true);
@@ -41,7 +94,14 @@ public class UIController : MonoBehaviour
         {
             Upgrade.gameObject.SetActive(false);
         }
+    }
 
+    /// <summary>
+    /// Check if spell to cast by player is choosen 
+    /// and show or not spell overlay vignette
+    /// </summary>
+    void SetCastVignette()
+    {
         if (_gameplayController.SpellToCast != -1)
         {
             CastingSpellVignette.gameObject.SetActive(true);
@@ -50,36 +110,58 @@ public class UIController : MonoBehaviour
         {
             CastingSpellVignette.gameObject.SetActive(false);
         }
-
-        TransportPartText.text = $"{_gameplayController.TransportPart*100}%";
-        SpeedText.text = $"x{_gameplayController.GameplaySpeedMultiplier}";
     }
 
+    void FixedUpdate()
+    {
+        SetPlayerUI();
+        SetUpgradeButton();
+        SetCastVignette();
+    }
+
+    /// <summary>
+    /// Triggered when button for upgrade is pressed
+    /// </summary>
     public void OnUpgrade()
     {
         _gameplayController.UpgradeVertex(_gameplayController.SelectedVertexA);
     }
 
+    /// <summary>
+    /// Triggered when button for offensive spell is pressed
+    /// </summary>
     public void OnOffensiveSpellButton()
     {
-        _gameplayController.SetSpellToCast(0);
-    }   
-        
-    public void OnEarthquakeSpellButton()
-    {
-        _gameplayController.SetSpellToCast(1);
-    }   
-        
-    public void OnTakeoverSpellButton()
-    {
-        _gameplayController.SetSpellToCast(2);
+        _gameplayController.SetSpellToCastByPlayer(0);
     }
 
+    /// <summary>
+    /// Triggered when button for earthquake spell is pressed
+    /// </summary>
+    public void OnEarthquakeSpellButton()
+    {
+        _gameplayController.SetSpellToCastByPlayer(1);
+    }
+
+    /// <summary>
+    /// Triggered when button for takeover spell is pressed
+    /// </summary>
+    public void OnTakeoverSpellButton()
+    {
+        _gameplayController.SetSpellToCastByPlayer(2);
+    }
+
+    /// <summary>
+    /// Triggered when button for pause menu is pressed
+    /// </summary>
     public void OnMenuButton()
     {
         _gameplayController.IsShowingMenu = true;
     }
 
+    /// <summary>
+    /// Triggered when button for army transport part is pressed
+    /// </summary>
     public void OnTransportPartButton()
     {
         float currentPart = _gameplayController.TransportPart;
@@ -101,6 +183,9 @@ public class UIController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Triggered when button for time scale speed is pressed
+    /// </summary>
     public void OnSpeedButton()
     {
         float currentSpeed = _gameplayController.GameplaySpeedMultiplier;
@@ -132,7 +217,5 @@ public class UIController : MonoBehaviour
                 _gameplayController.GameplaySpeedMultiplier = 0.5f;
                 break;
         }
-
-        Time.timeScale = _gameplayController.GameplaySpeedMultiplier;
     }
 }
